e2bec7fe4e8b21b6dab896b9d8dda90ea4abec3b
Andrew <andrewsmiththefirst@gmail.com>
2018-02-13 17:01:35 -0500

    almost done

--- controller.py
+++ controller.py
@@ -3,15 +3,14 @@ import time
 from model import Node_State_Model
 import os
 import datetime
-# execfile("model.py")
-execfile("node.py")
+execfile("model.py")
+# execfile("node.py")
 
 # Serial port may be different!
-# arduino = serial.Serial('/dev/cu.usbmodem1421', 9600)
+arduino = serial.Serial('/dev/cu.usbmodem1421', 9600)
 # connection takes 2 seconds to create
 time.sleep(2)
-# model = Node_State_Model()
-node = Node()
+model = Node_State_Model()
 
 # controlles the blah blah to do blah blah
 class StandardController:
@@ -21,45 +20,51 @@ class StandardController:
 
 # gets the state of a paticular port from the model
 # Port_Number as int -> binary int
-	# def getstate(self, port_number):
-	# 	return model.send_state(port_number)
+	def getstate_onoff(self, port_number):
+		return model.send_state_onoff(port_number)
 
 # gets the state of a nodes from the model
 # Null -> array of binary ints
-	# def getstates(self):
-	# 	return model.send_states()
+	def getstates_onoff(self):
+		return model.send_states_onoff()
+
+	def getstate_firing(self, port_number):
+		return model.send_state_firing(port_number)
+
+	def getstates_firing(self):
+		return model.send_states_firing()
 
 # toggle the state to paticular node
 # Port_Number as int -> null
-	def toggle_onoff(self):
-		node.toggle_onoff()
+	# def toggle_onoff(self):
+	# 	node.toggle_onoff()
+	def toggle_port(self, port_number):
+		model.toggle_port_onoff(port_number)
 
 # sets the state to paticular node to a given value
 # Port_Number as int, bool -> null
-	# def set_port_to(self, port_number, state):
-	# 	model.set_port_to(port_number, state)
+	def set_port_to(self, port_number, state):
+		model.set_port_to(port_number, state)
 
-	def get_frequency(self):
-		return float(1)/model.send_hertz()
+	def get_pulse_length(self,port_number):
+		return model.get_pulse_length(port_number)
 
-	def set_hertz(self):
-		hertz = input("Enter Hertz: ")
-		print(hertz)
-		model.set_hertz(hertz)
+	def get_wave_length(self,port_number):
+		return model.get_wave_lengths(port_number)
 
-	def get_pulse_lengths(self):
+	def get_pulse_length(self):
 		return model.get_pulse_lengths()
 
-	def get_wave_lengths(self):
+	def get_wave_length(self):
 		return model.get_wave_lengths()
 
-	def set_pulse_lengths(self,port_number,time):
+	def set_pulse_length(self,port_number,time):
 		model.set_pulse_lengths(port_number,time)
 
-	def set_wave_lengths(self,port_number,time):
+	def set_wave_length(self,port_number,time):
 		model.set_wave_lengths(port_number,time)
 
-	def set_delays(self):
+	def get_delays(self):
 		delays = []
 		pulses = self.get_pulse_lengths()
 		waves = self.get_wave_lengths()
@@ -78,7 +83,7 @@ class StandardController:
 			self.set_port_to(i,False)
 
 	def send_signal(self):
-		port = self.getstates()
+		port = self.getstates_firing()
 		if port[0]:
 			arduino.write("0")
 		else:
@@ -135,24 +140,20 @@ class StandardController:
 
 test_obj = StandardController()
 
-test_obj.set_delays()
-# times = []
-#
-# test_obj.set_hertz()
-# period = test_obj.get_frequency()
-# s = input("How many seconds do you want this to run?:")
-# q = int(s*(1/period))
-# print 1/period
-# t=time.time()
-# count = 0
-#
-# for x in range(0,q):
-# 	t+=period
-# 	while t > time.time():
-# 		pass
-# 	count = count + 1
-# 	test_obj.toggle_port(2)
-# 	test_obj.send_signal()
-#
+times = []
+period = .0001
+s = input("How many seconds do you want this to run?:")
+q = int(s*(1/period))
+print 1/period
+test_obj.toggle_port(8)
+test_obj.toggle_port(4)
+test_obj.set_pulse_length(8,.1)
+test_obj.set_wave_length(8,.3)
+test_obj.set_pulse_length(4,.25)
 
-# Default T = 500milliseconds DC = 200 microseconds
+t=time.time()
+for x in range(0,q):
+	t+=period
+	while t > time.time():
+		pass
+	test_obj.send_signal()
--- model.py
+++ model.py
@@ -1,53 +1,104 @@
 import numpy as np
 import pandas as pd
 
+class Node:
+	on_off = False
+	pulse_length = .0002
+	wave_length = .5
+	iteration = 0
+	system_period = .0001
+
+	def toggle_onoff(self):
+		if self.on_off == False:
+			print("toggled")
+			self.on_off = True
+		else:
+			self.on_off = False
+	def set_onoff(self,state):
+		self.on_off = state
+	def get_onoff(self):
+		return on_off
+
+	def get_wave_lengths(self):
+		return wave_length
+
+	def get_pulse_lengths(self):
+		return pulse_length
+
+	def set_system_period(self, system_period):
+		self.system_period = system_period
+
+	def get_system_period(self):
+		return self.system_period
+
+	def set_wave_length(self, x):
+		self.wave_length = x
+	def set_pulse_length(self,x):
+		self.pulse_length = x
+	def send_state(self):
+		# print(self.wave_length/self.system_period)
+		# print(self.iteration * self.system_period)
+		# print(self.pulse_length)
+		# print(self.iteration)
+		if self.on_off:
+			if (self.iteration*100) > (self.wave_length/self.system_period):
+				self.iteration = 0
+			if ((self.iteration*100) * self.system_period) <  self.pulse_length:
+				self.iteration = self.iteration + 1
+
+				return True
+			else:
+				self.iteration = self.iteration + 1
+				return False
+		else:
+			self.iteration = 0
+			return False
 #Node states
 class Node_State_Model():
-	port_numbers = 13
-	port_states = [False]*port_numbers
-	hertz = 60
-	pulse_lengths = [.0002]*port_numbers
-	wave_lengths = [.5]*port_numbers
+	port_numbers = 14
+	ports = []
+	for x in range(0,port_numbers):
+		ports.append(Node())
 	#sends the state of a paticular port
 	#Port_Number as int -> binary int
-	def send_state(self,port_number):
-		return self.port_states[port_number]
+	def send_state_firing(self,port_number):
+		return self.ports[port_number].send_state()
 
-	def read_csv(self):
-		df = pd.read_csv('states.csv', skiprows=[1],sep=',',header=None)
-	#sends the state of a nodes
-	#Null -> array of binary ints
-	def send_states(self):
-		df = pd.DataFrame(self.port_states)
-		df.to_csv("states.csv")
-		return list(self.port_states)
+	def send_states_firing(self):
+		return [self.ports[x].send_state() for x in range(1,self.port_numbers)]
+
+	def send_state_onoff(self,port_number):
+		return self.ports[port_number].get_onoff()
+
+	def send_states_onoff(self):
+		return [self.ports[x].get_onoff() for x in range(1,self.port_numbers)]
+
 	#toggle the state to paticular node
 	#Port_Number as int -> null
-	def toggle_port(self,port_number):
-		self.port_states[port_number] = not(self.port_states[port_number])
+	def toggle_port_onoff(self,port_number):
+		self.ports[port_number].toggle_onoff()
 	#sets the state to paticular node to a given value
 	#Port_Number as int, bool -> null
 	def set_port_to(self,port_number,state):
-		self.port_states[port_number] = state
-
-	def send_hertz(self):
-		print self.hertz
-		return self.hertz
-
-	def set_hertz(self, hertz):
-		self.hertz = hertz
+		self.ports[port_number].set_onoff(state)
 
 	def set_pulse_lengths(self,port_number,time):
-		self.pulse_lengths[port_number] = time
+		self.ports[port_number].set_pulse_length(time)
 
 	def set_wave_lengths(self,port_number,time):
-		self.wave_lengths[port_number] = time
+		self.ports[port_number].set_wave_length(time)
+
+	def get_pulse_length(self,port_number):
+		return self.ports[port_number].get_pulse_lengths()
+
+	def get_wave_length(self,port_number):
+		return self.self.ports[port_number].get_wave_lengths()
 
 	def get_pulse_lengths(self):
-		return self.pulse_lengths
+		return [self.ports[x].get_pulse_lengths() for x in range(1,self.port_numbers)]
 
 	def get_wave_lengths(self):
-		return self.wave_lengths
+		return [self.ports[x].get_wave_lengths() for x in range(1,self.port_numbers)]
 
 # test_class = Node_State_Model()
 # test_class.send_states()
